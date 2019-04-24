7f2133979367ee1feea0cfc37b81f7077a85cd28
kshanki <35243588+kshanki@users.noreply.github.com>
2019-04-24 00:37:01 -0400

    Create loadjobs.sh

---
+++ loadjobs.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Download and run XYZ script to populate queue with jobs/messages
+message=("A" "B" "C")
+Number_Jobs=${#message[@]}>Total_Job.txt
+for i in ${message[@]}
+do
+e="$(echo "$i" | base64)"
+python send.py $e
+done
